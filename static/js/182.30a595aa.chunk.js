65dd568f6e26ce4a559824126694d30e53aaeb21
vitaliipshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-06-23 13:10:50 +0000

    Deploying to gh-pages from @ vitaliipshenychnyi/prototype@8751a7a8740a628f7132df97c2b6642c2f698f3c 🚀

---
+++ static/js/182.30a595aa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[182],{182:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"PAGE 2"})}}}]);
+//# sourceMappingURL=182.30a595aa.chunk.js.map
